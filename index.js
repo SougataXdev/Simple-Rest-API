41f581db18d056c9fefc43cf91cfde04abe2329a
SougataXdev <iaamsougata@gmail.com>
2024-03-06 17:19:02 +0530

    added new post form

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const express = require("express");
-
 const app = express();
+const port = 8080;
+const path = require("path");
 
-const port =8080;
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true })); 
 
 let posts = [
     {
-        user:"sougata",
-        content:"I love coding"
+        user: "sougata",
+        content: "I love coding"
     },
     {
-        user:"sougata",
-        content:"I love coding"
+        user: "sougata",
+        content: "I love coding"
     },
     {
-        user:"sougata",
-        content:"I love coding"
+        user: "sougata",
+        content: "I love coding"
     }
 ];
 
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log("app is running");
 });
 
-app.set("view engine" ,"ejs" );
+app.set("view engine", "ejs");
 
-app.get("/posts" , (req , res)=>{
-    res.render("home.ejs" ,{posts} );
+app.get("/posts", (req, res) => {
+    res.render("home.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("newpost.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    const { user, content } = req.body; 
+    posts.push({user , content})
+    res.redirect("/posts");
 });
